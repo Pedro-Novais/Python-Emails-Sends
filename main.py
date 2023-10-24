778e5aee352426c81b7af790c6cb46b46657dd66
Pedro-Novais <phnovais7@gmail.com>
2023-10-24 15:56:33 -0300

    Quase finalizado

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import smtplib, ssl
+import openpyxl
+import os
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.application import MIMEApplication
 from email.mime.image import MIMEImage
 from string import Template
-import openpyxl
 
 wb = openpyxl.load_workbook('excel/teste_faturamento-teste-email-duplicado 1.xlsx')
 
@@ -31,13 +32,14 @@ for linha in aba:
             valor_title.append(valorT)
             i = 1
         if(posR > 1):
-            if(posR < 9):
+            if(posR < 10):
                 valor[posR-2].append(valorT)
 
 for i in range(len(valor)):
     save_email.append(valor[i][10])
 
 email_rep = []
+valor_rep = []
 cont = 0
 before_email = 0
 now_email = 0
@@ -50,185 +52,200 @@ for i in range(len(save_email)):
         now_email = save_email[i]
         if(now_email == before_email):
             cont = i - 1 
-            email_rep.append(valor[cont])
+            email_rep.append(valor[cont][10])
+            valor_rep.append(valor[cont])
             back = back + 1
             index_del.append(cont)
         elif(now_email != before_email):
                 if(back > 0):
                     cont = i - 1 
-                    email_rep.append(valor[cont])
+                    email_rep.append(valor[cont][10])
+                    valor_rep.append(valor[cont])
                     index_del.append(cont)
-                    back = -1
-           
+                    back = 0
+
+index_del.reverse() 
 for i in range(len(index_del)):
-    del save_email[index_del[0]]
-    del valor[index_del[0]]
+    print(index_del)
+    del save_email[index_del[i]]
+    del valor[index_del[i]]
 
-
-
-print(save_email)
-print()
+"""#print(save_email)
+#print()
 print(email_rep)
-print(len(email_rep))
+print('')
+print(valor_rep)
+print('')
 print(valor)
+print('')"""
+
+arq_falta = []
+def verification_pdf():
+    exi = 0
+    dir_arq = ['Boletos', 'Notas']
+    for i in range(3):
+        blt_not = []
+
+        blt_not.append(valor[i][1])
+        blt_not.append(valor[i][3])
+        for ini in range(2):
+            dir = f'pdf/{dir_arq[ini]}/{blt_not[ini]}.pdf'
+            if os.path.exists(dir):
+                print('')
+                print(f'{dir_arq[ini]}, de número {blt_not[ini]} existe')
+            else:
+                print('')
+                print(f'{dir_arq[ini]}, de número {blt_not[ini]} não existe')
+                arq_falta.append([dir_arq[ini], blt_not[ini]])
+                exi = -1
+    return exi
 
 def read_template(filename):
     with open(filename, 'r', encoding='utf-8') as template_file:
         template_file_content = template_file.read()
     return Template(template_file_content)
 
+if __name__ == '__main__':
+   stop = verification_pdf()
+
+print(stop)
+print(arq_falta)
+
+#state_email = 0
 def send_test_mail():
+    if(stop == 0):
+        #server = 'smtp.office365.com'
+        server = 'smtp-mail.outlook.com'
+        port = 587
+        sender_email = "phnovaisnew@outlook.com"
+        password = "Insano01$"
 
-    """for i in range(len(valor)):
-        save_email.append(valor[i][10])
+        for ini in range(3):
+            msg = MIMEMultipart('alternative')
+            receiver_email = f'teste{save_email[ini]}'
 
-    email_rep = []
-    cont = 0
-    before_email = 0
-    now_email = 0
-    back = 0
-    index_del = []
-
-    for i in range(len(save_email)):
-        if(i > 0):
-            before_email = save_email[i-1]
-            now_email = save_email[i]
-            if(now_email == before_email):
-                cont = i - 1 
-                email_rep.append(valor[cont])
-                back = back + 1
-                index_del.append(cont)
-            elif(now_email != before_email):
-                    if(back > 0):
-                        cont = i - 1 
-                        email_rep.append(valor[cont])
-                        index_del.append(cont)
-                        back = -1
+            file = read_template("template/ind.txt")
             
-    for i in range(len(index_del)):
-        del save_email[index_del[0]]
-        del valor[index_del[0]]"""
-    
-    #server = 'smtp.office365.com'
-    server = 'smtp-mail.outlook.com'
-    port = 587
-    sender_email = "phnovaisnew@outlook.com"
-    password = "Insano01$"
+            infos = ['','','','','','','','','','','']
+            data = 0
+            subject = ""
+            data_two = 0
+            valorB = 0
+            valorL = 0
+            
+            comment = '<!-- -->'
+            ver_email = 0
+            log_email = """<p style="font-family: 'Passions Conflict', cursive;"> Maria Escobar </p>"""
+            img_html = """<img src="https://i.postimg.cc/0yNyxsv6/assinatura-JPG.jpg" alt="Assinatura_E-deploy"/>"""
+            img_atm = 0
 
-    for ini in range(3):
-        msg = MIMEMultipart('alternative')
-        receiver_email = save_email[ini]
+            for i in range(len(infos)):
+                infos[i] = valor[ini][i]
+                if(i == 2):
+                    data = format(infos[2], "%d/%m/%Y")
+                if(i == 6):
+                    subject = f'FATURAMENTO E-DEPLOY - {infos[6]}'
+                if(i == 7):
+                    string = str(infos[7])
+                    change = string.replace('.',',')
+                    if(len(change) < 6):
+                        valorB = f'R${change}0'
+                    else:
+                        valorB = f'R${change}'
+                if(i == 8):
+                    string = str(infos[8])
+                    change = string.replace('.',',')
+                    if(len(change) < 6):
+                        valorL = f'R${change}0'
+                    else:
+                        valorL = f'R${change}'
+                if(i == 9):
+                    data_two = format(infos[9], "%d/%m/%Y")
+                if(ver_email == 0):
+                    for i in range(len(receiver_email)):
+                        if(receiver_email[i] == 'g'):
+                            if(receiver_email[i+1] == 'm'):
+                                if(receiver_email[i+2] == 'a'):
+                                    if(receiver_email[i+3] == 'i'):
+                                        if(receiver_email[i+4] == 'l'):
+                                            img_atm = 1
+                                            ver_email = 1
+                                            log_email = img_html
+            blt = infos[1]
+            nota = infos[3]
 
-        file = read_template("template/ind.txt")
+            tmp_html = {
+                'ID':infos[0],
+                'BLT': blt,
+                'DATA_EMISSAO':data,
+                'NOTA': nota,
+                'RAZAO': infos[4],
+                'CNPJ': infos[5],
+                'DESCR': infos[6],
+                'VALORB': valorB,
+                'VALORL': valorL,
+                'DATA_VENC': data_two,
+                'IMAGEM': log_email,
+                #'EMAIL': infos[10]
+                'INFOS2': comment,
+                'INFOS3': comment,
+                'INFOS4': comment,
+                'INFOS5': comment,
+                'INFOS6': comment,
+                'INFOS7': comment,
+                'INFOS8': comment
+                }
+            
+            tmp = file.safe_substitute(tmp_html)
+            msg.attach(MIMEText(tmp, 'html'))
+
+            title_file = [f'Nota-Fiscal-{nota}.pdf', f'Boleto-{blt}.pdf']
+            pasta_blt = f'{blt}.pdf'
+            pasta_nota = f'{nota}.pdf'
+            pasta_dir_blt = f'Boletos'
+            pasta_dir_notas = f'Notas'
+            pasta = ""
+            pasta_arq = ""
+            state = 0
+            
+            for num in range(2):
+                if(state == 0):
+                    pasta = pasta_dir_notas
+                    pasta_arq = pasta_nota
+                    state = 1
+                elif(state == 1):
+                    pasta = pasta_dir_blt
+                    pasta_arq = pasta_blt
+                pdf = MIMEApplication(open(f'pdf/{pasta}/{pasta_arq}', 'rb').read())
+                pdf.add_header('Content-Disposition', 'attachment', filename= title_file[num])
+                msg.attach(pdf)
+
+            if(img_atm != 1):
+                with open('assinatura.jpg', 'rb') as fp:
+                    img = MIMEImage(fp.read())
+                    img.add_header('Content-Disposition', 'attachment', filename="assinatura.jpg")
+                    msg.attach(img)
         
-        infos = ['','','','','','','','','','','']
-        data = 0
-        subject = ""
-        data_two = 0
-        valorB = 0
-        valorL = 0
-        
-        ver_email = 0
-        log_email = """<p style="font-family: 'Passions Conflict', cursive;"> Maria Escobar </p>"""
-        img_html = """<img src="https://i.postimg.cc/0yNyxsv6/assinatura-JPG.jpg" alt="Assinatura_E-deploy"/>"""
-        img_atm = 0
+            msg['Subject'] = subject
+            msg['From'] = sender_email
+            msg['To'] = receiver_email
 
-        for i in range(len(infos)):
-            infos[i] = valor[ini][i]
-            if(i == 2):
-                data = format(infos[2], "%d/%m/%Y")
-            if(i == 6):
-                subject = f'FATURAMENTO E-DEPLOY - {infos[6]}'
-            if(i == 7):
-                string = str(infos[7])
-                change = string.replace('.',',')
-                if(len(change) < 6):
-                    valorB = f'R${change}0'
-                else:
-                    valorB = f'R${change}'
-            if(i == 8):
-                string = str(infos[8])
-                change = string.replace('.',',')
-                if(len(change) < 6):
-                    valorL = f'R${change}0'
-                else:
-                    valorL = f'R${change}'
-            if(i == 9):
-                data_two = format(infos[9], "%d/%m/%Y")
-            if(ver_email == 0):
-                for i in range(len(receiver_email)):
-                    if(receiver_email[i] == 'g'):
-                        if(receiver_email[i+1] == 'm'):
-                            if(receiver_email[i+2] == 'a'):
-                                if(receiver_email[i+3] == 'i'):
-                                    if(receiver_email[i+4] == 'l'):
-                                        img_atm = 1
-                                        ver_email = 1
-                                        log_email = img_html
-        blt = infos[1]
-        nota = infos[3]
+            context = ssl.create_default_context()
 
-        tmp_html = {
-            'ID':infos[0],
-            'BLT': blt,
-            'DATA_EMISSAO':data,
-            'NOTA': nota,
-            'RAZAO': infos[4],
-            'CNPJ': infos[5],
-            'DESCR': infos[6],
-            'VALORB': valorB,
-            'VALORL': valorL,
-            'DATA_VENC': data_two,
-            'IMAGEM': log_email
-            #'EMAIL': infos[10]
-            }
-        
-        tmp = file.safe_substitute(tmp_html)
-        msg.attach(MIMEText(tmp, 'html'))
-
-        title_file = [f'Nota-Fiscal-{nota}.pdf', f'Boleto-{blt}.pdf']
-        pasta_blt = f'{blt}.pdf'
-        pasta_nota = f'{nota}.pdf'
-        pasta_dir_blt = f'Boletos'
-        pasta_dir_notas = f'Notas'
-        pasta = ""
-        pasta_arq = ""
-        state = 0
-        for num in range(2):
-            if(state == 0):
-                pasta = pasta_dir_notas
-                pasta_arq = pasta_nota
-                state = 1
-            elif(state == 1):
-                pasta = pasta_dir_blt
-                pasta_arq = pasta_blt
-            pdf = MIMEApplication(open(f'pdf/{pasta}/{pasta_arq}', 'rb').read())
-            pdf.add_header('Content-Disposition', 'attachment', filename= title_file[num])
-            msg.attach(pdf)
-
-        if(img_atm != 1):
-            with open('assinatura.jpg', 'rb') as fp:
-                img = MIMEImage(fp.read())
-                img.add_header('Content-Disposition', 'attachment', filename="assinatura.jpg")
-                msg.attach(img)
-      
-        msg['Subject'] = subject
-        msg['From'] = sender_email
-        msg['To'] = receiver_email
-
-        context = ssl.create_default_context()
-
-        try:
-            with smtplib.SMTP(server, port) as smtpObj:
-                smtpObj.ehlo()
-                smtpObj.starttls(context=context)
-                smtpObj.ehlo()
-                smtpObj.login(sender_email, password)
-                smtpObj.sendmail(sender_email, receiver_email, msg.as_string())
-                print(f"E-mail: {receiver_email} enviado,")
-        except Exception as e:
-            print(f"E-mail: {receiver_email}, falha ao enviar")
-            print(e)
+            try:
+                with smtplib.SMTP(server, port) as smtpObj:
+                    smtpObj.ehlo()
+                    smtpObj.starttls(context=context)
+                    smtpObj.ehlo()
+                    smtpObj.login(sender_email, password)
+                    smtpObj.sendmail(sender_email, receiver_email, msg.as_string())
+                    
+                    print(f"E-mail: {receiver_email} enviado,")
+            except Exception as e:
+                print(f"E-mail: {receiver_email}, falha ao enviar")
+                print(e)
+    else:
+        print(f'Boletos/Notas não encontrados: {arq_falta}')
 
 if __name__ == '__main__':
     send_test_mail()
